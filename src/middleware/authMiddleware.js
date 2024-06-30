e328d90928be71625b5d0e5cae1b304b7030b15a
sandunjay <sandunjayawardhana2001@gmail.com>
2024-07-01 00:41:16 +0530

    "Updated .env.example file with new variables and values, and modified authMiddleware.js to include token validation and logging"

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import dotenv from 'dotenv';
+import logger from '../utils/logger';
+import Token from '../models/Token';
 
 dotenv.config();
 
@@ -14,15 +16,23 @@ export const protect = async(req, res, next) => {
         try {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-password');
 
+            const storedToken = await Token.findOne({ token });
+            if (!storedToken || storedToken.revoked) {
+                logger.error(`Access token ${token} is revoked or expired`)
+                return res.status(401).json({ message: 'Unauthorized - Access token is revoked or expired.' });
+            }
+
+            req.user = await User.findById(decoded.id).select('-password');
             next();
         } catch (error) {
+            logger.error("Not authorized. Token failed")
             res.status(401).json({message: "Not authorized. Token failed"})
         }
     }
 
     if(!token){
-        res.status(401).json({message: "Not authorized. No token available"});
+        logger.error("No token available")
+        res.status(401).json({message: "No token available"});
     }
 }
